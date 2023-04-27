042b6bcc5df8198c5ad884e73f23cb15f58b16a3
biswasbittu <biswasbittu7415@yahoo.com>
2023-04-27 20:43:47 +0530

    navbar almost done

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,20 +1,48 @@
 import React from "react";
 import logo from "../../../assets/logo.png";
-import moment from 'moment';
+import moment from "moment";
+import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
+import Home from './../../Home/Home/Home';
 
 const Header = () => {
   return (
-    <div>
+    <Container>
       <div className="text-center">
         <img src={logo} alt="" />
         <p className="text-secondary">
           <small>Journalism Without Fear or Favour</small>
         </p>
-        <p>
-          {moment().format("dddd, MMMM D, YYYY")}
-        </p>
+        <p>{moment().format("dddd, MMMM D, YYYY")}</p>
       </div>
-    </div>
+      <div className="d-flex">
+        <Button variant="danger">Latest</Button>
+        <Marquee speed={70} className="text-danger">
+          I can be a React component, multiple React components, or just some
+          text.
+        </Marquee>
+      </div>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+      <Container>
+       
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
+            <Nav.Link href="#pricing">Career</Nav.Link>
+            
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Profiles</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+            <Button variant="secondary">Login</Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </Container>
   );
 };
 
